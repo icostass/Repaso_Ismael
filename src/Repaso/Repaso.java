d690915c5da84cd9d91202eba8debd90d49af8b8
costa <costa@IsmaPortatil>
2022-05-04 12:16:12 +0200

    Repaso

---
+++ src/Repaso/Repaso.java
@@ -0,0 +1,18 @@
+package Repaso;
+
+import java.util.Scanner;
+
+public class Repaso {
+
+	public static void main(String[] args) {
+		Scanner ent = new Scanner(System.in);
+		int pk;
+		
+		System.out.println("Elige un pokemon inciial: " + "\n\t" + "1) Charmander" +
+				"\n\t" + "2) Squirtle" + "\n\t" + "3) Bulbasur");
+			pk = ent.nextInt();
+		
+		//Haz un método con switch que ponga "Has elegido a X pokemon", sustituyendo la X por el pokemon elegido
+	}
+
+}
