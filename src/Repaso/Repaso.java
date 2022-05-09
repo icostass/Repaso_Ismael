747fb3f6f987bb1daaeeed27222b0beed47c8a0a
davis <davis@Portatil_David.mshome.net>
2022-05-09 08:46:02 +0200

    Commit de David

--- src/Repaso/Repaso.java
+++ src/Repaso/Repaso.java
@@ -8,11 +8,27 @@ public class Repaso {
 		Scanner ent = new Scanner(System.in);
 		int pk;
 		
-		System.out.println("Elige un pokemon inciial: " + "\n\t" + "1) Charmander" +
-				"\n\t" + "2) Squirtle" + "\n\t" + "3) Bulbasur");
+		System.out.println("Elige un pokemon inicial: " + "\n\t" + "1) Charmander" +
+				"\n\t" + "2) Squirtle" + "\n\t" + "3) Bulbasaur");
 			pk = ent.nextInt();
 		
 		//Haz un método con switch que ponga "Has elegido a X pokemon", sustituyendo la X por el pokemon elegido
+			
+		switch(pk) {
+			case 1:
+				System.out.println("Has elegido a Charmander");
+				break;
+			case 2:
+				System.out.println("Has elegido a Squirtle");
+				break;
+			case 3:
+				System.out.println("Has elegido a Bulbasaur");
+				break;
+			default:
+				System.out.println("Pokemon incorrecto");
+				break;
+				
+		}
 	}
 
 }
